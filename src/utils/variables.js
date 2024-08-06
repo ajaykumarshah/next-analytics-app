fa8257efe1a8aea938442b3ca8a3297ff7c6e7d5
Ajay kumar Shah <AKShah@bpl.netlink.com>
2024-08-07 00:24:36 +0530

    file added

---
+++ src/utils/variables.js
@@ -0,0 +1,7 @@
+const dateRegex = /\b((\d{1,2})(st|nd|rd|th)?[-\/](\d{1,2}|\w+)[-\/](\d{2,4})|(\d{4})[-\/](\d{1,2})[-\/](\d{1,2})|(\w+)\s+(\d{1,2})(st|nd|rd|th)?,\s*(\d{4})|(\d{1,2})(st|nd|rd|th)?\s+(\w+)\s+(\d{4}))\b/gi;
+
+
+
+
+
+export {dateRegex}
